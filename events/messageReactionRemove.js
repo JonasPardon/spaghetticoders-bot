592e944196baafb0afc9df6b99cc42f57133cff0
ThePjpollie <pieterjan.pollie@gmail.com>
2018-07-27 17:32:49 +0200

    Role removing based on reactions

---
+++ events/messageReactionRemove.js
@@ -0,0 +1,17 @@
+const moment = require('moment');
+
+module.exports = async (client, messageReaction, user) => {
+    
+    // * If it's not the defined reaction channel, ignore the reaction
+    if(messageReaction.message.channel !== client.settings.reactionChannel) return;
+    
+    // * Log the event
+    const time = moment().format('MMMM Do YYYY, hh:mm:ss');
+
+    console.log(`#### Reaction removed [${time}]`);
+    console.log(`   # Reaction: ${messageReaction._emoji.name}`);
+    console.log(`   # User: ${user.tag}`);
+
+    client.removeRoleFromReaction(user, messageReaction._emoji.name);
+
+}
--- modules/roles.js
+++ modules/roles.js
@@ -9,11 +9,29 @@ module.exports = async client => {
         if(!role) return;
 
         // * Return if the member already has the role
-        if(member.roles.has(role)) return;
+        if(member.roles.has(role.id)) return;
 
         await member.addRole(role);
         
         console.log(`   # Role ${role.name} given to ${member.user.tag}`);
+    }
+
+
+    client.removeRoleFromReaction = async (user, reactionName) => {
+
+        const member = await client.guild.members.get(user.id);
+
+        // * Get the role and return if it doesn't exist
+        const role = await client.guild.roles.get(client.config.roles[reactionName]);
+        if(!role) return;
+
+        // * Return if the member doesn't have the role
+
+        if(!member.roles.has(role.id)) return;
+
+        await member.removeRole(role);
+        
+        console.log(`   # Role ${role.name} removed from ${member.user.tag}`);
     }
 
 }
