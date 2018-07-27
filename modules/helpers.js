773735031e1c05100093670c15615e713297993a
Jonas Pardon <jonaspardon@hotmail.com>
2018-07-27 14:15:55 +0200

    getting rid of unnecesary comments

--- modules/helpers.js
+++ modules/helpers.js
@@ -2,17 +2,8 @@ module.exports = async client => {
 
     client.loadCommand = (commandName) => {
         try {
-            // const props = require(`../commands/${commandName}`);
             const command = require(`../commands/${commandName}`);
             console.log(` - Loaded Command: ${command.help.name}. 👌`);
-            // if (props.init) {
-            //     props.init(client);
-            // }
-            // client.commands.set(props.help.name, props);
-            // props.conf.aliases.forEach(alias => {
-            //     client.aliases.set(alias, props.help.name);
-            // });
-
             client.commands[commandName.substring(0, commandName.length - 3)] = command;
 
             return false;
